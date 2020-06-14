3170ad623a04bf700c1a34218227df78ccacc22b
christianacuna <45090069+christianacuna@users.noreply.github.com>
2020-06-14 13:57:19 -0600

    Update grafobipartido.pl
    se pudo arreglar el problema de grafobipartido

--- grafobipartido.pl
+++ grafobipartido.pl
@@ -9,11 +9,66 @@
 
 maximoSubgrafoBipartito([],_ListaAristas,[]).
 maximoSubgrafoBipartito(_ListaNodos,[],[]).
-maximoSubgrafoBipartito(ListaNodos,ListaAristas,Resultado):-!.
+maximoSubgrafoBipartito(_ListaNodos,ListaAristas,Resultado):-crearGrafosConListaArista(ListaAristas,Resultado).
+
+checkeoLogico(N,L,R):- member(N,L) -> R = true; R = false.
+
+accRev([H|T],A,R):- accRev(T,[H|A],R).
+accRev([],A,A).
+
+rev(L,R):- accRev(L,[],R).
+
+%Nodos no existen en los grafos entonces se agregan.
+compuertaLogica(false,false,false,false,N1,N2,L1,L2,NL1,NL2,Bandera):- append(L1,[N1],NL1), append(L2,[N2],NL2),Bandera = false.
+
+%Nodos si existen en los grafos e invalidan requerimiento bipartido.
+compuertaLogica(true,true,true,true,_N1,_N2,L1,L2,L1,L2,Bandera):- Bandera = true.
+compuertaLogica(true,false,true,false,_N1,_N2,L1,L2,L1,L2,Bandera):- Bandera = true.
+compuertaLogica(false,true,false,true,_N1,_N2,L1,L2,L1,L2,Bandera):- Bandera = true.
+
+%Nodos si existen en los grafos pero no invalidan requerimiento bipartido.
+compuertaLogica(true,false,false,true,_N1,_N2,L1,L2,L1,L2,Bandera):- Bandera = false.
+compuertaLogica(false,true,true,false,_N1,_N2,L1,L2,L1,L2,Bandera):- Bandera = false.
+
+%Nodo1 si existe en los grafos, se inserta Nodo2 en el otro grafo.
+compuertaLogica(true,false,false,false,_N1,N2,L1,L2,L1,NL2,false):- append(L2,[N2],NL2).
+compuertaLogica(false,true,false,false,_N1,N2,L1,L2,NL1,L2,false):- append(L1,[N2],NL1).
+
+%Nodo2 si existe en los grafos, se inserta Nodo1 en el otro grafo.
+compuertaLogica(false,false,true,false,N1,_N2,L1,L2,L1,NL2,false):- append(L2,[N1],NL2).
+compuertaLogica(false,false,false,true,N1,_N2,L1,L2,NL1,L2,false):- append(L1,[N1],NL1).
+
+% insertarUnicoEnGrafos([a,b],[],[],NG1,NG2,R).
+insertarUnicoEnGrafos([N1|[N2|_]],G1,G2,NG1,NG2,R):- %write('New'),
+	checkeoLogico(N1,G1,R1),%write(R1),
+	checkeoLogico(N1,G2,R2),%write(R2),
+	checkeoLogico(N2,G1,R3),%write(R3),
+	checkeoLogico(N2,G2,R4),%write(R4),
+	compuertaLogica(R1,R2,R3,R4,N1,N2,G1,G2,NG1,NG2,R),!. %print(NG1) , print(NG2),write(R),print('\n'),!.
+
+%Cambiar esta lina en caso de usar Backtracking.
+%crearGrafosConListaAristaAux(_,_Grafo1,_Grafo2,[]).
+crearGrafosConListaAristaAux([],_Grafo1,_Grafo2,[]).
+crearGrafosConListaAristaAux([Cabeza|Cola],Grafo1,Grafo2,NewListaArista):-
+	%print(Cola),print('\n'),
+	insertarUnicoEnGrafos(Cabeza,Grafo1,Grafo2,NewGrafo1,NewGrafo2,Bandera),
+	(\+Bandera-> 
+		(crearGrafosConListaAristaAux(Cola,NewGrafo1,NewGrafo2,NLA), append([Cabeza],NLA,NewListaArista)) ;
+		(\+(NewListaArista = _) -> (crearGrafosConListaAristaAux(Cola,NewGrafo1,NewGrafo2,[]));
+			(crearGrafosConListaAristaAux(Cola,NewGrafo1,NewGrafo2,NewListaArista)))).
+		
+% append([Cabeza],NewListaArista,NLA)
+%crearGrafosConListaArista([[a,b],[a,c],[b,c],[b,d],[c,d]],L).
+
+crearGrafosConListaArista([],[]).
+crearGrafosConListaArista(ListaArista,NewListaArista):- crearGrafosConListaAristaAux(ListaArista,[],[],NewListaArista).
+
+insertarUnicoLista(Elemento,[],[Elemento]).
+insertarUnicoLista(Elemento,Lista,NewLista):- \+ member(Elemento,Lista) -> NewLista = [Elemento|Lista] ; fail.
 
 esBipartito([],_ListaAristas,false).
 esBipartito(_ListaNodos,[],false).
-esBipartito(ListaNodos,ListaAristas,Resutlado):- 
+esBipartito(ListaNodos,ListaAristas,_Resutlado):- 
 	esGrafoConexo(ListaNodos,ListaAristas,EsConexo), 
 	(EsConexo -> Resultado = true; Resultado = false).
 
@@ -22,7 +77,7 @@ esGrafoConexo(ListaNodos,ListaAristas,Resultado):-
 	sort(ListaNodosDuplicados2,ListaNodos2), 
 	(listaIgual(ListaNodos,ListaNodos2) -> Resultado = true ; Resultado = false).
 
-esGrafoAutoDirigido(ListaNodos,ListaAristas,Resultado):- maplist(subListaDiferentes,ListaAristas).
+%esGrafoAutoDirigido(_ListaNodos,_ListaAristas,_Resultado):- maplist(subListaDiferentes,ListaAristas).
 
 enterosIgual(Numero,Numero2):- Numero=:=Numero2.
 
@@ -31,14 +86,3 @@ subListaDiferentes([Cabeza|[Cabeza2|_]]):- \+(nodosIguales(Cabeza,Cabeza2)).
 nodosIguales(Nodo,Nodo2):- Nodo = Nodo2.
 
 listaIgual(L1, L2) :- maplist(=, L1, L2).
-
-miembro(E,[E|_T]).
-miembro(E,[_H|T]):-miembro(E,T).
-
-camino(G,A,B,[A,B]):-miembro([A,B],G).
-camino(G,_A,_B,Camino):- 
-camino(G,A,B,[A|Camino]):-camino(G,C,B,Camino),miembro([A,C],G),\+ miembro(A,Camino).
-
-esCaminoValido(G,[A,B]):-miembro([A,B],G).
-esCaminoValido(G,[A,B|Cam]):- 
-            miembro([A,B],G), esCaminoValido(G,[B|Cam]).
