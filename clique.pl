13f2ac8b598e663f22af0c6b12fc090c234a3fb5
ssg-13 <sebas.mqt@gmail.com>
2020-06-14 23:25:29 -0600

    pequeño cambio en el codigo de clique

--- clique.pl
+++ clique.pl
@@ -1,4 +1,13 @@
-%[a,b,c,d], [[a,b],[a,c],[b,c],[b,d],[c,d]], X
+%[a,b,c,d], [[a,b],[a,c],[b,c],[b,d],[c,d]]
+%[a,b,c,d], [[a,b],[c,d]]
+%[a,b,c,d], [[a,b],[a,c],[c,d]]
+%[a,b,c,d], [[b,c],[b,d],[c,d]]
+%[a,b,c,d], [[a,d],[b,c]]
+%[a,b,c,d,e,f], [[a,b],[a,c],[b,c],[b,d],[c,d]]
+%[a,b,c,d,e,f], [[a,b],[a,c],[b,c],[b,d],[c,d],[a,e],[a,f]]
+%[a,b,c,d,e,f], [[a,b],[a,c],[b,c],[b,d],[c,d],[b,e],[b,f]]
+%[a,b,c,d,e,f], [[a,b],[a,c],[b,c],[b,d],[c,d],[c,e],[c,f]]
+%[a,b,c,d,e,f], [[a,b],[a,c],[b,c],[b,d],[c,d],[d,e],[d,f]]
 
 combinaciones(_L,0,[]).
 combinaciones([H|T],N,[H|C]):-N>0, N1 is N-1, combinaciones(T,N1,C).
@@ -16,5 +25,4 @@ verificarCliqueAux(RN,VN):-forall(member(E,RN), member(E,VN)).
 cliqueMaximo(LN,LA,X):-length(LN,L), L1 is L-1, buscarCliques(LN,LA,L1,X).
 
 buscarCliques(_LN,_LA,1,_X):-!.
-buscarCliques(LN,LA,L,C):-combinaciones(LN,L,C), verificarClique(C,LA).
-buscarCliques(LN,LA,L,X):-L1 is L-1, buscarCliques(LN,LA,L1,X).
+buscarCliques(LN,LA,L,C):-(combinaciones(LN,L,C), verificarClique(C,LA)) ; (L1 is L-1, buscarCliques(LN,LA,L1,C)).
