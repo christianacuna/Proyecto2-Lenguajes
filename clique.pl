b6f8e4661247283f2e8ce2efc8ace943b8e39c71
ssg-13 <sebas.mqt@gmail.com>
2020-06-14 20:19:15 -0600

    se agrega relacion para encontrar todos los cliques

---
+++ clique.pl
@@ -0,0 +1,20 @@
+%[a,b,c,d], [[a,b],[a,c],[b,c],[b,d],[c,d]], X
+
+combinaciones(_L,0,[]).
+combinaciones([H|T],N,[H|C]):-N>0, N1 is N-1, combinaciones(T,N1,C).
+combinaciones([_H|T],N,C):-N>0, combinaciones(T,N,C).
+
+vecinosConN(_N,[],[]):-!.
+vecinosConN(N,[[N,B]|A],[B|RV]):-vecinosConN(N,A,RV),!.
+vecinosConN(N,[[B,N]|A],[B|RV]):-vecinosConN(N,A,RV),!.
+vecinosConN(N,[_C|A],RV):-vecinosConN(N,A,RV),!.
+
+verificarClique([N|RN],LA):-vecinosConN(N,LA,VN), verificarCliqueAux(RN,VN).
+
+verificarCliqueAux(RN,VN):-forall(member(E,RN), member(E,VN)).
+
+cliqueMaximo(LN,LA,X):-length(LN,L), L1 is L-1, buscarCliques(LN,LA,L1,X).
+
+buscarCliques(_LN,_LA,1,_X):-!.
+buscarCliques(LN,LA,L,C):-combinaciones(LN,L,C), verificarClique(C,LA).
+buscarCliques(LN,LA,L,X):-L1 is L-1, buscarCliques(LN,LA,L1,X).
